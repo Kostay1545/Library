9150c564ccfe27522bd2b65d7e326f831a71e59a
Kostay1545 <kostikbaryshev10@gmail.com>
2025-10-02 18:42:46 +0300

    add Book.cpp file

---
+++ Book.cpp
@@ -0,0 +1,10 @@
+//
+// Created by User on 02.10.2025.
+//
+#include <iostream>
+#include "Book.h"
+
+void Book::printInfo(title, author, year) {
+    std::cout << "Title:" << title;
+    std::cuot << "Author:" << author;
+    std::cout << "Year:" << year;
